fc6a0496ba72311ccfd81aec3ec56b29b19acf27
Simone Baravelli <simobara@hotmail.it>
2023-07-28 12:17:09 +0200

    UpdateService

--- src/DataAPI/services/auth.service.js
+++ src/DataAPI/services/auth.service.js
@@ -1,24 +1,42 @@
-import axios from "axios";
+import { useEffect, useState } from "react";
+import AuthService from "./path/to/AuthService"; // Assicurati di fornire il percorso corretto al file AuthService
 
-const API_URL = "http://localhost:8080/api/auth/";
+function MyComponent() {
+  // Definisci uno stato per immagazzinare i dati dell'utente corrente
+  const [currentUser, setCurrentUser] = useState(null);
 
-class AuthService {
-  login = async (username, password) => {
-    const response = await axios.post(API_URL + "signin", { username, password });
-    if (response.data.accessToken) localStorage.setItem("user", JSON.stringify(response.data));
-    return response.data;
+  // Esegui l'effetto una sola volta al momento del montaggio del componente
+  useEffect(() => {
+    // Richiama la funzione di AuthService per ottenere l'utente corrente
+    const user = AuthService.getCurrentUser();
+    setCurrentUser(user);
+  }, []);
+
+  // Funzione per gestire l'azione di logout
+  const handleLogout = () => {
+    AuthService.logout();
+    setCurrentUser(null);
   };
 
-  logout = () => {
-    localStorage.removeItem("user");
-  };
-
-  register = async (username, email, password) => {
-    const response = await axios.post(API_URL + "signup", { username, email, password });
-    return response.data;
-  };
-
-  getCurrentUser = () => JSON.parse(localStorage.getItem("user"));
+  // Restituisce il contenuto del componente
+  return (
+    <div>
+      {currentUser ? (
+        // Se l'utente è loggato, visualizza i dettagli dell'utente e un pulsante di logout
+        <div>
+          <h1>Benvenuto, {currentUser.username}</h1>
+          <p>Email: {currentUser.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        // Se l'utente non è loggato, visualizza un messaggio di login
+        <div>
+          <h1>Effettua il login</h1>
+          {/* Aggiungi qui i campi e la logica del form per il login */}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default new AuthService();
+export default MyComponent;
