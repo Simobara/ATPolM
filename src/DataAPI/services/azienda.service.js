413fc6469b4add4714b811b4922716cb441d90f6
Simone Baravelli <simobara@hotmail.it>
2023-08-01 12:35:46 +0200

    fixing services

--- src/DataAPI/services/azienda.service.js
+++ src/DataAPI/services/azienda.service.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/views/';
@@ -6,20 +6,53 @@ const API_URL = 'http://localhost:8080/views/';
 const AziendaService = () => {
     const [aziende, setAziende] = useState([]);
 
-    useEffect(() => {
-        const fetchAziende = async () => {
-            try {
-                const response = await axios.get(API_URL + 'aziende-all');
-                setAziende(response.data);
-            } catch (error) {
-                console.error('Error while fetching aziende:', error);
+    const getAziende = async () => {
+        try {
+            const response = await axios.get(API_URL + 'aziende-all');
+            return response.data;
+        } catch (error) {
+            console.error('Error while fetching aziende:', error);
+        }
+    };
+
+
+
+    const addAziende = async (descrizione) => {
+        try {
+            const response = await axios.post(API_URL + 'add-aziende', {
+                descrizione,
+            });
+
+            if (response.data.accessToken) {
+                localStorage.setItem('aziende', JSON.stringify(response.data));
             }
-        };
 
-        fetchAziende();
-    }, []);
+            return response.data;
+        } catch (error) {
+            console.error('Error while adding aziende:', error);
+        }
+    };
 
-    return aziende;
+    const deleteAziende = async (id) => {
+        try {
+            const response = await axios.delete(`${API_URL}aziende/${id}`, {
+                id,
+            });
+
+            if (response.data.accessToken) {
+                localStorage.setItem('aziende', JSON.stringify(response.data));
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error('Error while deleting aziende:', error);
+        }
+    };
+
+
+
+
+    return { getAziende, addAziende, deleteAziende };
 };
 
 export default AziendaService;
